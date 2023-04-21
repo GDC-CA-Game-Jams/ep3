7429af9d954738fcdbd76bd95bdf7bd942a0527b
Alex Zahn <alexzahn@yahoo.com>
2023-04-20 18:22:45 -0700

    UI Clock now has hourly and EoD callbacks

--- CA Jam 3 Unity Project/Assets/Scripts/UI/UI_Clock.cs
+++ CA Jam 3 Unity Project/Assets/Scripts/UI/UI_Clock.cs
@@ -10,10 +10,17 @@ public class UI_Clock : MonoBehaviour
     private float minuteHandDegreesPerSecond;
     private float hourHandDegreesPerSecond;
 
-    private float realtimeSecondsElapsed = 60f * 9f;
+    private float initialRealtimeSecondsElapsed = 60f * 9f;
+    private float realtimeSecondsElapsed;
+
+    private int numHoursElapsed;
+
+    private bool isClockPaused = false;
 
     private void Awake()
     {
+        realtimeSecondsElapsed = initialRealtimeSecondsElapsed;
+
         minuteHandDegreesPerSecond = 360f / 60f;
         hourHandDegreesPerSecond = minuteHandDegreesPerSecond / 12f;
 
@@ -21,11 +28,42 @@ public class UI_Clock : MonoBehaviour
         hourHandTransform   = transform.Find("ClockHourHand");
     }
 
+    private void Start()
+    {
+        // Call the onHourElapsed() function once for each in-game hour
+        InvokeRepeating("onHourElapsed", 60f, 60f);
+    }
+
     private void Update()
     {
-        realtimeSecondsElapsed += Time.deltaTime;
+        if (!isClockPaused)
+        {
+            realtimeSecondsElapsed += Time.deltaTime;
 
-        minuteHandTransform.eulerAngles = new Vector3(0, 0, -realtimeSecondsElapsed * minuteHandDegreesPerSecond);
-        hourHandTransform.eulerAngles = new Vector3(0, 0, -realtimeSecondsElapsed * hourHandDegreesPerSecond);
+            minuteHandTransform.eulerAngles = new Vector3(0, 0, -realtimeSecondsElapsed * minuteHandDegreesPerSecond);
+            hourHandTransform.eulerAngles = new Vector3(0, 0, -realtimeSecondsElapsed * hourHandDegreesPerSecond);
+        }
+    }
+
+    private void onHourElapsed()
+    {
+        numHoursElapsed++;
+
+        print(numHoursElapsed + "hours elapsed.");
+
+        // Play clock chime
+
+        if(numHoursElapsed == 8)
+        {
+            print("End of workday!");
+
+            isClockPaused = true;
+
+            // Play end-of-day clock chime
+
+            // Other end-of-day logic goes here
+
+            CancelInvoke();
+        }
     }
 }
