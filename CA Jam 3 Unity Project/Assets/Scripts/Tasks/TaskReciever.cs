1b2b706412a6d841e5a7b51a29b6484ed23cdc73
Alex Zahn <alexzahn@yahoo.com>
2023-05-02 22:45:14 -0700

    Green arrows over task drop off locations

--- CA Jam 3 Unity Project/Assets/Scripts/Tasks/TaskReciever.cs
+++ CA Jam 3 Unity Project/Assets/Scripts/Tasks/TaskReciever.cs
@@ -12,6 +12,8 @@ public class TaskReciever : MonoBehaviour
     // if maxTimesUpdateTask = 1, the player can only drop off 1 package here
     // if maxTimesUpdateTask = 4, the player can drop off 4 packages here
 
+    [SerializeField] private GameObject greenArrow;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && timesRemainingUpdateTask >= 1)
@@ -22,6 +24,8 @@ public class TaskReciever : MonoBehaviour
             if (completed)
             {
                 timesRemainingUpdateTask -= 1; //decrease the amount of times the player can complete the task at this location again
+
+                greenArrow.SetActive(false); //remove the green arrow
             }
             
         }
