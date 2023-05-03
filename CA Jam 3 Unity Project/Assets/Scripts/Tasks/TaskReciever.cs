ca1f49db39b51211ffdf099064ba7d6dd538511e
JulieBodette <jr3python@gmail.com>
2023-05-02 23:56:05 -0400

    Player can only complete a task once at each location
    Number of times they can complete a task at a location can be configured- I set it to 1 by default.

--- CA Jam 3 Unity Project/Assets/Scripts/Tasks/TaskReciever.cs
+++ CA Jam 3 Unity Project/Assets/Scripts/Tasks/TaskReciever.cs
@@ -7,11 +7,16 @@ using UnityEngine;
 public class TaskReciever : MonoBehaviour
 {
     [SerializeField] private TaskSO task;
-    
+    [SerializeField] private int timesRemainingUpdateTask = 1; //the maximum number of times a task can be updated at this task reciever
+    // example: let's say the task reciever is a package drop off location
+    // if maxTimesUpdateTask = 1, the player can only drop off 1 package here
+    // if maxTimesUpdateTask = 4, the player can drop off 4 packages here
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && timesRemainingUpdateTask >= 1)
         {
+            timesRemainingUpdateTask -= 1; //decrease the amount of times the player can complete the task at this location again
             ServiceLocator.Instance.Get<TaskManager>().UpdateTask(task);
         }
     }
