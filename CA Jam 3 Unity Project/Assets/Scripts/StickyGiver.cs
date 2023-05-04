5c4c409bde3f159f8ab7a8212be48de92dccc082
JulieBodette <jr3python@gmail.com>
2023-05-04 00:50:52 -0400

    Fixed StickyGiver Dialogue

--- CA Jam 3 Unity Project/Assets/Scripts/StickyGiver.cs
+++ CA Jam 3 Unity Project/Assets/Scripts/StickyGiver.cs
@@ -12,12 +12,37 @@ public class StickyGiver : MonoBehaviour
     [SerializeField]
     private bool given = false;
 
+    [Tooltip("The speech bubble that appears when the NPC is talking")]
+    [SerializeField] private GameObject speechBubble;
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player" && !given)
         {
             ServiceLocator.Instance.Get<TaskManager>().AssignTask(sticky);
+            speechBubble.SetActive(true);
             given = true;
+
+            //stun the player- keep them from moving
+            //code copied from NPCChatter
+            PlayerMovement player = (PlayerMovement)other.gameObject.GetComponent("PlayerMovement");
+            player.MaxSpeedMod = -10;
+            player.MoveForceMod = -500;
+            speechBubble.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            speechBubble.SetActive(false);
+
+            //allow player to move
+            //code copied from NPCChatter
+            PlayerMovement player = (PlayerMovement)other.gameObject.GetComponent("PlayerMovement");
+            player.MaxSpeedMod = 0;
+            player.MoveForceMod = 0;
         }
     }
 }
