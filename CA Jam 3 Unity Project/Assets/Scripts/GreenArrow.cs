3ddddab5410ed6e28914022fe19fb04685af8a14
Alex Zahn <alexzahn@yahoo.com>
2023-05-08 23:07:40 -0700

    Green Arrow + DayStartListener fixes

--- CA Jam 3 Unity Project/Assets/Scripts/GreenArrow.cs
+++ CA Jam 3 Unity Project/Assets/Scripts/GreenArrow.cs
@@ -7,10 +7,12 @@ public class GreenArrow : MonoBehaviour
     [SerializeField] GameObject itemBase;
     [SerializeField] GameObject greenArrow;
 
+    public float upDisplacement = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        greenArrow.transform.SetPositionAndRotation(itemBase.transform.position + Vector3.up * 4, Quaternion.LookRotation(Vector3.right));
+        greenArrow.transform.SetPositionAndRotation(itemBase.transform.position + Vector3.up * upDisplacement, Quaternion.LookRotation(Vector3.right));
     }
 
     // Update is called once per frame
